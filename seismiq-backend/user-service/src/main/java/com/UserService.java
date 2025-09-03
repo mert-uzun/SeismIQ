5dfa4973f40eabb7149e00c8c4e7683172617cd8
Ayşe Ece <ece.bilgi@ug.bilkent.edu.tr>
2025-09-03 12:17:42 +0300

    Update UserService and User model

--- seismiq-backend/user-service/src/main/java/com/UserService.java
+++ seismiq-backend/user-service/src/main/java/com/UserService.java
@@ -2,6 +2,8 @@ package com;
 
 import java.util.UUID;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import com.seismiq.common.model.User;
 import com.seismiq.user.UserRepository;
 
@@ -19,10 +21,12 @@ public class UserService {
         this.userRepository = new UserRepository();
     } 
     
-    public void registerUser(User user){
-        if(user.getUserId() == null){
-            user.setUserId(UUID.randomUUID().toString());
+    public void registerUser(User user) throws Exception{
+        if(userRepository.getUserByEmail(user.getEmail()) != null){
+            throw new Exception("Email already registered");
         }
+        user.setUserId(UUID.randomUUID().toString());
+        user.setPasswordHash(BCrypt.hashpw(user.getPasswordHash(), BCrypt.gensalt()));
         userRepository.saveUser(user);
     }
 
@@ -37,4 +41,12 @@ public class UserService {
     public void deleteUserProfile(String userId){
         userRepository.deleteUser(userId);
     }
+    
+    public User loginUser(String email, String password) throws Exception {
+        User user = userRepository.getUserbyEmail(email);
+        if(user == null || ! BCrypt.checkpw(password, user.getPasswordHash())){
+            throw new Exception("Invalid emial or password");
+        }
+        return user;
+    } 
 }
