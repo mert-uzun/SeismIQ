8b93f51b8e11eee6ca5c7983f225793144030954
Mert Uzun <mert.uzun@ug.bilkent.edu.tr>
2025-10-04 00:35:36 +0300

    Update seismiq-frontend from feature-branch

--- seismiq-frontend/app/src/main/java/com/seismiq/app/SeismIQApplication.java
+++ seismiq-frontend/app/src/main/java/com/seismiq/app/SeismIQApplication.java
@@ -1,6 +1,7 @@
 package com.seismiq.app;
 
 import android.app.Application;
+import android.content.Context;
 import android.util.Log;
 
 import com.amplifyframework.AmplifyException;
@@ -9,10 +10,13 @@ import com.amplifyframework.core.Amplify;
 
 public class SeismIQApplication extends Application {
 
+    private static SeismIQApplication instance;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        
+        instance = this;
+
         try {
             // Initialize Amplify for AWS Cognito authentication
             Amplify.addPlugin(new AWSCognitoAuthPlugin());
@@ -21,5 +25,9 @@ public class SeismIQApplication extends Application {
         } catch (AmplifyException error) {
             Log.e("SeismIQApp", "Could not initialize Amplify", error);
         }
+    }
+
+    public static Context getAppContext() {
+        return instance.getApplicationContext();
     }
 }
